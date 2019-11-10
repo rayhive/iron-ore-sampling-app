fcca473ba40b13ba4b64ac614d3f2cae57e09562
Reuben Amoako-Atta <yawatta@gmail.com>
2019-11-10 15:55:22 +0800

    Added skeletal ASP.NET Core Web API project.

---
+++ IronOreSamplingApi/Controllers/IronOreSamplingController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IronOreSamplingApi.Controllers
+{
+    [ApiController]
+    [Route("api/samples")]
+    public class IronOreSamplingController : ControllerBase
+    {
+        private static readonly string[] Samples = new[]
+        {
+            "Sample A", "Sample B"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Samples;
+        }
+    }
+}
